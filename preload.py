bce00ea75d5dc9abec08cf244f6f8a5149fb01af
woricek <53396311+woricek@users.noreply.github.com>
2025-03-19 17:31:24 +0100

    Update preload.py

--- preload.py
+++ preload.py
@@ -1,5 +1,3 @@
 
 def preload(parser):
     parser.add_argument("--wildcards-dir", type=str, help="directory with wildcards", default=None)
-    parser.add_argument("--wildcards-start", type=str, help="index of first item to take", default=None)
-    parser.add_argument("--wildcards-count", type=str, help="number of items to take", default=None)
